f71cc5342bfd5e54059cdff0a41065a9bd85c5f2
jatin mishra <jatinmishra@jatins-MacBook-Pro.local>
2021-03-11 20:09:27 -0700

    create pwa with react

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// we are simply rendering element id named root into the dom.
